e809f24258ceb58c83b45198fb9f3698b336c977
Yuri Tveritin <tveritin.ya@udmapk.ru>
2017-10-30 22:31:38 +0400

    Добавлено ожидание подготовки всех машин CountDownLatch и реализована очередь в туннеле определение победителя

--- src/j3hw5race/Car.java
+++ src/j3hw5race/Car.java
@@ -7,10 +7,11 @@ package j3hw5race;
 
 import static j3hw5race.J3hw5race.cdl;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
 
 public class Car implements Runnable {
     private static int CARS_COUNT;
-
+    
     static {
         CARS_COUNT = 0;
     }
@@ -18,8 +19,10 @@ public class Car implements Runnable {
     private Race race;
     private int speed;
     private String name;
+    CyclicBarrier cb;
     CountDownLatch cdl;
-    int win;
+//    int win;
+    public    volatile int win =0;//победитель
 
     public String getName() {
         return name;
@@ -28,26 +31,31 @@ public class Car implements Runnable {
     public int getSpeed() {
         return speed;
     }
+    public void win(){
+        if (win==0) {
+            System.out.println (this.name+" ВЫИГРАЛ");
+            win=1;
+        }
+    }
 
-    public Car(Race race, int speed, CountDownLatch cdl,int win) {//конструктор класса
+    public Car(Race race, int speed, CyclicBarrier cb,CountDownLatch cdl) {//конструктор класса
         this.race = race;
         this.speed = speed;
         CARS_COUNT++;
         this.name = "Участник #" + CARS_COUNT;
+        this.cb=cb;
         this.cdl=cdl;
         this.win=win;
     }
 
     @Override
-    public void run() {
+    public synchronized void run() {
         try {
             System.out.println(this.name + " готовится");
             Thread.sleep(500 + (int)(Math.random() * 800));
-            System.out.println(this.name + " готов");
-            cdl.countDown();
-            cdl.await();     
-            
-//            this.wait();
+            System.out.println(this.name + " готов");            
+            cb.await();
+            cb.await();
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -56,10 +64,11 @@ public class Car implements Runnable {
         for (int i = 0; i < race.getStages().size(); i++) {
             race.getStages().get(i).go(this);
         }
-        if (win==0) {
-            System.out.println (this.name+" ВЫИГРАЛ");
-            win=1;
-        }
+        cdl.countDown();
+        if (cdl.getCount()==3) win();
+        
+        
+
     }
 }
 
