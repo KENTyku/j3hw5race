9ebf3e28d6889c091909d4bf820409d11adc434d
Yuri Tveritin <tveritin.ya@udmapk.ru>
2017-10-30 22:32:08 +0400

    Добавлено ожидание подготовки всех машин CountDownLatch и реализована очередь в туннеле определение победителя

--- src/j3hw5race/J3hw5race.java
+++ src/j3hw5race/J3hw5race.java
@@ -11,7 +11,11 @@
 // и добавлять объекты классов из пакета util.concurrent
 package j3hw5race;
 
+import java.util.ArrayList;
+import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.Semaphore;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -21,24 +25,53 @@ import java.util.logging.Logger;
  */
 public class J3hw5race {
     public static final int CARS_COUNT = 4;
-    public static CountDownLatch cdl=new CountDownLatch(CARS_COUNT);//счетчик подготовки потоков(машин)
-    public static void main(String[] args) {
+    public static CyclicBarrier cb = new CyclicBarrier(CARS_COUNT+1);//счетчик подготовки потоков(машин)
+    public static CountDownLatch cdl = new CountDownLatch(CARS_COUNT);
+    public static Semaphore smp=new Semaphore(CARS_COUNT/2);
+    public static ArrayList<Thread> thrds= new ArrayList();
+
+    public static synchronized void main(String[] args) {
         
         System.out.println("Важное объявление>>>>Подготовка");
-        Race race = new Race(new Road(60), new Tunnel(), new Road(40));
-        Car[] cars = new Car[CARS_COUNT];
+        Race race = new Race(new Road(60), new Tunnel(smp), new Road(40));
+        Car[] cars = new Car[CARS_COUNT];//массив машин
         for (int i = 0; i < cars.length; i++) {
-            cars[i] = new Car(race, 20 + (int) (Math.random() * 10),cdl);
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10),cb,cdl);
         }
         for (int i = 0; i < cars.length; i++) {
-            new Thread(cars[i]).start();
+            thrds.add(new Thread(cars[i]));//создаем список потоков (для машин)
+            thrds.get(i).start();//каждую машину запускаем отдельным потоком
         }
+        
+        
+        try {
+            cb.await();
+        } catch (InterruptedException ex) {
+            Logger.getLogger(J3hw5race.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (BrokenBarrierException ex) {
+            Logger.getLogger(J3hw5race.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
+        
+        
+
+        System.out.println("Важное объявление>>>>Гонка началась!");
+        try {
+            cb.await();            
+        } catch (InterruptedException ex) {
+            Logger.getLogger(J3hw5race.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (BrokenBarrierException ex) {
+            Logger.getLogger(J3hw5race.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
         try {
             cdl.await();
         } catch (InterruptedException ex) {
             Logger.getLogger(J3hw5race.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        System.out.println("Важное объявление>>>>Гонка началась!");
+        }      
+            
+        
         System.out.println("Важное объявление>>>>Гонка закончилась!");
     }
 }
--- src/j3hw5race/Tunnel.java
+++ src/j3hw5race/Tunnel.java
@@ -1,9 +1,14 @@
 package j3hw5race;
 
+import java.util.concurrent.Semaphore;
+
 public class Tunnel extends Stage {
-    public Tunnel() {
+    Semaphore smp;
+    public Tunnel(Semaphore smp) {
         this.length = 80;
         this.description = "Тоннель " + length + " метров";
+        this.smp=smp;
+        
     }
 
     @Override
@@ -11,12 +16,14 @@ public class Tunnel extends Stage {
         try {
             try {
                 System.out.println(c.getName() + " готовится к этапу(ждет): " + description);
+                smp.acquire();
                 System.out.println(c.getName() + " начал этап: " + description);
                 Thread.sleep(length / c.getSpeed() * 1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             } finally {
                 System.out.println(c.getName() + " закончил этап: " + description);
+                smp.release();
             }
         } catch (Exception e) {
             e.printStackTrace();
